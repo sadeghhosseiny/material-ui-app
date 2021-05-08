fecc90307a065ef6808d13642f1a2b242afcd884
sadegh <thesadegh.h@gmail.com>
2021-05-08 23:04:32 +0430

    add something else

--- material-ui-app/src/Components/Navbar.js
+++ material-ui-app/src/Components/Navbar.js
@@ -8,27 +8,29 @@ import MenuIcon from "@material-ui/icons/Menu";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
 import InputBase from "@material-ui/core/InputBase";
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Drawer from '@material-ui/core/Drawer';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
-import List from '@material-ui/core/List';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import HomeIcon from "@material-ui/icons/Home";
+import { ListItemIcon } from "@material-ui/core";
 
 const useStyle = makeStyles((theme) => ({
   superRoot: {
     width: "280px",
-    transition: "0.4s ease-in-out"
+    transition: "0.4s ease-in-out",
   },
   FocusedSuperRoot: {
     width: "480px",
-    transition: "0.4s ease-in-out"
+    transition: "0.4s ease-in-out",
   },
   root: {
     position: "relative",
     width: "100%",
-    marginLeft:"35px",
+    marginLeft: "35px",
   },
   searchInput: {
     width: "calc(100% - 85px)",
@@ -41,33 +43,28 @@ const useStyle = makeStyles((theme) => ({
     color: "#333",
     borderRadius: "4px",
     margin: "8px",
-    transition: "0.4s ease-in-out"
+    transition: "0.4s ease-in-out",
   },
   btnContainer: {
     position: "absolute",
     top: "12px",
     left: "16px",
-    
   },
   icon: {
     padding: "7px",
-    transition:"0.4s ease-in-out",
+    transition: "0.4s ease-in-out",
 
-      color:"red",
-      transform:"rotate(-360deg)",
-    
+    color: "red",
+    transform: "rotate(-360deg)",
   },
 
   icon2: {
     padding: "7px",
-    transition:"0.4s ease-in-out",
-    
+    transition: "0.4s ease-in-out",
   },
-
 
   menuButton: {
     marginRight: "5px",
-
   },
 
   loginButton: {
@@ -89,36 +86,51 @@ const useStyle = makeStyles((theme) => ({
     backgroundImage: "linear-gradient(45deg, #FFC312, #EE5A24, #00A8FF)",
   },
 
-  list:{
+  list: {
     // background:"red",
-    width:"250px",    
+    width: "250px",
   },
 
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     // padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
-    transition:"0.4s ease-in-out",
+    transition: "0.4s ease-in-out",
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-    transition: theme.transitions.create('margin', {
+    justifyContent: "flex-end",
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
   },
-
 }));
 
 function Navbar() {
   const classes = useStyle();
   const [isFocused, setIsFocused] = useState(false);
-  const [open, setOpen] = useState({left: false});
+  const [open, setOpen] = useState({ left: false });
+
+  const ItemList = [
+    {
+      text: "Home",
+      icon: <HomeIcon color="primary" />
+    },
+    {
+      text: "Page1",
+    },
+    {
+      text: "Page2",
+    },
+    {
+      text: "Page3",
+    },
+  ];
 
   const handleDrawer = (side, open) => (event) => {
-    setOpen({...open, [side]:open})
-    console.log("shit", open)
-  }
+    setOpen({ ...open, [side]: open });
+    console.log("shit", open);
+  };
 
   return (
     <div>
@@ -128,13 +140,14 @@ function Navbar() {
             className={classes.menuButton}
             edge="start"
             color="inherit"
-            onClick={handleDrawer("left",true)}
-
+            onClick={handleDrawer("left", true)}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6">News</Typography>
-          <div className={isFocused ? classes.FocusedSuperRoot : classes.superRoot}>
+          <div
+            className={isFocused ? classes.FocusedSuperRoot : classes.superRoot}
+          >
             <div className={classes.root}>
               <InputBase
                 className={classes.searchInput}
@@ -145,7 +158,9 @@ function Navbar() {
                 onChange={(e) => e.target.value}
               />
               <span className={classes.btnContainer}>
-                <IconButton className={isFocused ? classes.icon : classes.icon2}>
+                <IconButton
+                  className={isFocused ? classes.icon : classes.icon2}
+                >
                   <SearchRoundedIcon />
                 </IconButton>
               </span>
@@ -157,29 +172,31 @@ function Navbar() {
           </Button>
         </Toolbar>
       </AppBar>
-      
 
-      { open && 
-      <Drawer transitionDuration={500} open={open.left} >
-        <div className={classes.drawerHeader}>
-
-        <IconButton onClick={handleDrawer("left", false)}>
-        <ChevronLeftIcon />
-
-          </IconButton>
-        </div>
-      <List className={classes.list}>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-          
-        </List>
-      </Drawer>}
+      {open && (
+        <Drawer transitionDuration={500} open={open.left}>
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawer("left", false)}>
+              <ChevronLeftIcon />
+            </IconButton>
           </div>
-   
+          <Divider />
+          <List className={classes.list}>
+            {ItemList.map((item, index) => {
+              // const {text, icon} = item;
+              return (
+                <ListItem button key={"li" + index}>
+                  {/* or you can do that in this way but you should destructor the item to text -> line 191 */}
+                  {/* <ListItemText primary={text} /> */}
+                  {item.icon && <ListItemIcon>{item.icon}</ListItemIcon>}
+                  <ListItemText>{item.text}</ListItemText>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Drawer>
+      )}
+    </div>
   );
 }
 
