8897ab52c0c805589446cabfe16d97d77699e2f4
sadegh <thesadegh.h@gmail.com>
2021-05-11 15:54:37 +0430

    add form and formik material ui

--- material-ui-app/src/Components/LoginPage.js
+++ material-ui-app/src/Components/LoginPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+// import TextField from '@material-ui/core/TextField';
+import {Formik, Form, Field} from 'formik';
+import {TextField} from 'formik-material-ui';
 
 const useStyle = makeStyles((theme) => ({
         textField: {
@@ -11,7 +13,7 @@ const useStyle = makeStyles((theme) => ({
             marginTop: 0,
             fontWeight: 500,
             "& .MuiInput-underline:after": {
-                borderBottomColor: "blue",
+                borderBottomColor: "purple",
                 borderBottom:"3px solid"
               },
               "& .MuiInput-underline:before":{
@@ -29,25 +31,60 @@ const useStyle = makeStyles((theme) => ({
 function LoginPage() {
     const classes = useStyle();
     return (
-        <div>
-           <div className={classes.Text}>
-               <h1>Material-Ui</h1>
-           </div>
-           <form >
-           <TextField
-    id="email"
-    label="Email"
-    className={classes.textField}
-
-    margin="normal"
-    InputProps={{
-        className: classes.input,
-    }}
-/>
-           </form>
-
-        </div>
-    )
+        
+        <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.com/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        // onSubmit={(values, { setSubmitting }) => {
+        //   setTimeout(() => {
+        //     setSubmitting(false);
+        //     alert(JSON.stringify(values, null, 2));
+        //   }, 500);
+        // }}
+      >
+        {/* {({ submitForm, isSubmitting }) => ( */}
+          <Form>
+            <Field
+              component={TextField}
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+            />
+            <br />
+            <Field
+              component={TextField}
+              type="password"
+              label="Password"
+              name="password"
+            />
+            {/* {isSubmitting && <LinearProgress />} */}
+            <br />
+            {/* <Button
+              variant="contained"
+              color="primary"
+              disabled={isSubmitting}
+              onClick={submitForm}
+            >
+              Submit
+            </Button> */}
+          </Form>
+        {/* )} */}
+      </Formik>
+    );
 }
 
 export default LoginPage
