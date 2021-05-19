3a02f3713228573626f776d3b2bf1f3218de82e3
sadegh <thesadegh.h@gmail.com>
2021-05-19 21:22:32 +0430

    find some new value

--- material-ui-app/src/Components/Home.js
+++ material-ui-app/src/Components/Home.js
@@ -1,28 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
+
 function Home() {
 
     const [posts, setPosts] = useState([]);
-    const temp = [];
+
     useEffect(() => {
         axios.get(`https://jsonplaceholder.typicode.com/posts`)
         .then(res => {
-            
             let i = 0;
             //console.log("LEN",len);
             res.data.forEach(data => {
                 if(i <= 50){
-                   //console.log(data.length);
-                    temp.push(data);
+                    setPosts(prev => [...prev, data]);
                     i++;
-                    setPosts([...temp]);
                 }
             }
             )
-            console.log("posts",posts);
-            console.log("TEMP", temp);
-                //console.log(data);
     
         })
         .catch(error => {
@@ -30,11 +25,11 @@ function Home() {
         })
     }, [])
 
-
     return (
         <div>
+            {console.log(posts)}
             <ol>
-                {temp.map(po => {
+                {posts.map(po => {
                     return <li key={po.id}>{po.title}</li>
                 })}
             </ol>
